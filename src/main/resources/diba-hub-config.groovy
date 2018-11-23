3761dca2d495a6308625b454b145721a8dc84541
Karla Rubio Sopan <>
2018-11-23 23:02:19 +0100

    add new configuration files in the classpath, remove unneccesary autogenerated files

---
+++ src/main/resources/diba-hub-config.groovy
@@ -0,0 +1,32 @@
+mapdownloads {
+    baseLayers {
+        default_layer {            
+            i18nCode = 'baselayer.provincia'
+            name = 'ESP_adm2'
+            displayName = 'Provincias'
+        }
+        world {            
+            i18nCode = 'baselayer.world'
+            name = 'world'
+            displayName = 'Esquema mundial'
+        }        
+    }
+    baseMaps {
+        hybrid {
+            i18nCode = 'basemap.hybrid'
+            name = 'hybrid'
+            displayName = 'Google Hybrid'
+        }
+        satellite {
+            i18nCode = 'basemap.satellite'
+            name = 'satellite'
+            displayName = 'Google Satellite'
+        }
+        roadmap {
+            i18nCode = 'basemap.hybrid'
+            name = 'roadmap'
+            displayName = 'Google Road Map'
+        }
+    }
+}
+
